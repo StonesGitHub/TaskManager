ddab8e6f2c24f5a0b1ea64b2929af6771aa50972
StoneGitHub <bubugao0809@163.com>
2015-11-08 19:48:25 +0800

    Task Manager first code from home

---
+++ TaskManager/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class TaskItem
+    {
+        public string TaskName { get; set; }
+
+        public string TaskNote { get; set; }
+    }
+}
---
+++ TaskManager/TaskViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager
+{
+    public class TaskViewModel
+    {
+        private ObservableCollection<TaskItem> _taskCollections;
+
+        public TaskViewModel()
+        {
+            _taskCollections = new ObservableCollection<TaskItem>();
+        }
+
+        public ObservableCollection<TaskItem> TaskCollections 
+        {
+            get { return _taskCollections; }
+            set { _taskCollections= value; }
+        }
+    }
+}
